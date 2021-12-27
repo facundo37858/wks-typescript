a9e6bccf4c561775d74c551f98baae918601b2aa
facundo2309 <facundoomargarcia@gmail.com>
2021-12-27 17:55:21 -0300

    coneccion database

---
+++ App/wks-typescript/api/index.ts
@@ -0,0 +1,16 @@
+
+import app from "./src/app";
+import { sequelize } from "./src/db";
+
+interface error{
+	err:string
+}
+sequelize
+	.sync({force: true, logging: false})
+	.then(() => {
+		console.log('base de datos conectada! :D')
+		app.listen(3001, function () {
+			console.log('App is listening on port 3001!');
+		});
+	})
+	.catch((err:error) => console.error(err));
